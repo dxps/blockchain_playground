533a83666c812ec6fa06678a0ee47398e39f3404
dxps <marius.ileana@gmail.com>
2021-10-23 22:47:35 +0300

    notes upd

---
+++ notes/Solana.mm
@@ -0,0 +1,119 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Solana" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1635012929804" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#3c3836">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font NAME="Hasklig" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font NAME="Hasklig"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18.75 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="Hasklig" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font NAME="Hasklig"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#fbf1c7">
+<font NAME="Hasklig" SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT">
+<font NAME="Hasklig"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Hasklig"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#458588" BORDER_COLOR_ALPHA="38">
+<font NAME="Hasklig"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font NAME="Hasklig"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_749235638" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_749235638"/>
+<font NAME="Hasklig" SIZE="12" ITALIC="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font NAME="Hasklig"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<font NAME="Hasklig" SIZE="18"/>
+<node TEXT="Dev Workflows" FOLDED="true" POSITION="right" ID="ID_1406678249" CREATED="1635013173216" MODIFIED="1635013181049">
+<node TEXT="build Programs" ID="ID_1170948670" CREATED="1635013183850" MODIFIED="1635013448594">
+<node TEXT="written in Rust, C, or C++" ID="ID_798849875" CREATED="1635013226026" MODIFIED="1635013235084"/>
+<node TEXT="these are smart contracts" ID="ID_619368414" CREATED="1635013260045" MODIFIED="1635013267459"/>
+<node TEXT="are on-chain apps" ID="ID_1806655333" CREATED="1635013422175" MODIFIED="1635013436348"/>
+</node>
+<node TEXT="build dApps" ID="ID_854901585" CREATED="1635013449828" MODIFIED="1635013453368">
+<node TEXT="are external apps that interact with the blockchain&#xa;through an SDK or directly via the JSON RPC API." ID="ID_1778338268" CREATED="1635013466617" MODIFIED="1635013579347"/>
+<node TEXT="these apps send transactions with instructions&#xa;to on-chain programs." ID="ID_1640999012" CREATED="1635013601137" MODIFIED="1635013638001"/>
+</node>
+</node>
+<node TEXT="Programs (SC)" POSITION="right" ID="ID_1962513767" CREATED="1635013681430" MODIFIED="1635013698613">
+<node TEXT="there are two sets" ID="ID_273927373" CREATED="1635013707877" MODIFIED="1635013925219" VGAP_QUANTITY="3 pt">
+<node TEXT="Native Programs" ID="ID_133127595" CREATED="1635013904343" MODIFIED="1635013925219" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="-8.25 pt">
+<node TEXT="are the core of Solana." ID="ID_1274839205" CREATED="1635013725327" MODIFIED="1635013883209"/>
+<node TEXT="one of the most common ones is System program&#xa;(used for creating accounts, transferring SOL, and others)." ID="ID_149392146" CREATED="1635013785271" MODIFIED="1635013891848"/>
+<node TEXT="other popular ones: Staking and Voting." ID="ID_1364816182" CREATED="1635013942937" MODIFIED="1635013979844"/>
+</node>
+<node TEXT="Solana Program Library (SPL)" ID="ID_375973769" CREATED="1635013730333" MODIFIED="1635013756272">
+<node TEXT="Token program is the most popular included)" ID="ID_302625142" CREATED="1635014000690" MODIFIED="1635014113241">
+<node TEXT="is Solana&apos;s Ethereum ERC-20 equivalent." ID="ID_299087681" CREATED="1635014114729" MODIFIED="1635014133178"/>
+<node TEXT="allows interacting with tokens, including NFTs." ID="ID_1056572504" CREATED="1635014141228" MODIFIED="1635014165370"/>
+<node TEXT="allows minting, transferring, burning or querying tokens." ID="ID_491171440" CREATED="1635014185054" MODIFIED="1635014220358"/>
+<node TEXT="sample using it: spl-token-wallet" ID="ID_1009649532" CREATED="1635014284306" MODIFIED="1635014326501" LINK="https://github.com/project-serum/spl-token-wallet">
+<node TEXT="is a React wallet app" ID="ID_473649222" CREATED="1635014349995" MODIFIED="1635014360514"/>
+<node TEXT="uses solana-web3.js SDK" ID="ID_586998733" CREATED="1635014360900" MODIFIED="1635014450381" LINK="https://github.com/solana-labs/solana-web3.js"/>
+<node TEXT="uses the Token program (using Token Program JS Bindings)&#xa;for creating token accounts and transferring tokens." ID="ID_494145758" CREATED="1635014401068" MODIFIED="1635014466273" LINK="https://github.com/solana-labs/solana-program-library/blob/master/token/js/client/token.js"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ notes/Substrate.mm
@@ -0,0 +1,145 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Substrate" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1634844894482" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#3c3836">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_gruvbox_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font NAME="Hasklig" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font NAME="Hasklig"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-19.5 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="Hasklig" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font NAME="Hasklig"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#fbf1c7">
+<font NAME="Hasklig" SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT">
+<font NAME="Hasklig"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Hasklig"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#458588" BORDER_COLOR_ALPHA="38">
+<font NAME="Hasklig"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font NAME="Hasklig"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_749235638" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_749235638"/>
+<font NAME="Hasklig" SIZE="12" ITALIC="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font NAME="Hasklig"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0" COLOR="#ffba00" BACKGROUND_COLOR="#282828" BACKGROUND_ALPHA="0" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" MAX_WIDTH="17 cm">
+<font NAME="Hasklig" SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<font NAME="Hasklig" SIZE="18"/>
+<node TEXT="Runtime" POSITION="right" ID="ID_1329524343" CREATED="1634838502396" MODIFIED="1634844894482" HGAP_QUANTITY="14.75 pt" VSHIFT_QUANTITY="-26.25 pt">
+<node TEXT="Primitives" ID="ID_1506081609" CREATED="1634839407600" MODIFIED="1634839413566">
+<node TEXT="Core" ID="ID_105897351" CREATED="1634839415954" MODIFIED="1634839418075">
+<node TEXT="Hash" ID="ID_1600123647" CREATED="1634839420215" MODIFIED="1634839422149"/>
+<node TEXT="DigestItem" ID="ID_1095420769" CREATED="1634839422570" MODIFIED="1634839428481"/>
+<node TEXT="Digest" ID="ID_1574918797" CREATED="1634839428966" MODIFIED="1634839431165"/>
+<node TEXT="Extrinsic" FOLDED="true" ID="ID_99994309" CREATED="1634839435781" MODIFIED="1634839441736">
+<node TEXT="is" ID="ID_868211036" CREATED="1634841162364" MODIFIED="1634841168376">
+<node TEXT="external data that is recognized by the blockchain." ID="ID_194483172" CREATED="1634841170923" MODIFIED="1634841246871"/>
+<node TEXT="typically involves one or more signatures and encoded instructions&#xa;(e.g. transferring ownership of funds or calling into a smart contract)" ID="ID_1271624812" CREATED="1634841255049" MODIFIED="1634841327724"/>
+</node>
+<node TEXT="falls into 3 categories" ID="ID_1772146369" CREATED="1634841516191" MODIFIED="1634841529934">
+<node TEXT="Inherents" FOLDED="true" ID="ID_636427644" CREATED="1634841551396" MODIFIED="1634842882114">
+<node TEXT="are" ID="ID_484064880" CREATED="1634842591950" MODIFIED="1634842627520">
+<node TEXT="pieces of information that are not signed and&#xa;only inserted into a block by the block author." ID="ID_812173286" CREATED="1634842628652" MODIFIED="1634842683051"/>
+<node TEXT="ex: the author of the block may insert a timestamp inherent into the block." ID="ID_405531733" CREATED="1634842773421" MODIFIED="1634842830809"/>
+</node>
+</node>
+<node TEXT="Signed transactions" FOLDED="true" ID="ID_320046315" CREATED="1634841556523" MODIFIED="1634842887593">
+<node TEXT="contain a signature of the account that issued the transaction&#xa;and stands to pay a fee to have the transaction included on chain." ID="ID_150348842" CREATED="1634842929645" MODIFIED="1634842990400"/>
+<node TEXT="fit the concept of a transaction in Ethereum or Bitcoin." ID="ID_1419480608" CREATED="1634843026345" MODIFIED="1634843041880"/>
+</node>
+<node TEXT="Unsigned transactions" ID="ID_1920122936" CREATED="1634841563703" MODIFIED="1634842893186"/>
+</node>
+</node>
+<node TEXT="Header" ID="ID_1228796137" CREATED="1634839442183" MODIFIED="1634839444136"/>
+<node TEXT="Block" ID="ID_1247500038" CREATED="1634839444423" MODIFIED="1634839446026"/>
+<node TEXT="BlockNumber" ID="ID_1864289718" CREATED="1634839451921" MODIFIED="1634839455508"/>
+</node>
+<node TEXT="FRAME" ID="ID_582864320" CREATED="1634839460792" MODIFIED="1634839464221">
+<node TEXT="Pallet" ID="ID_1277756225" CREATED="1634839486707" MODIFIED="1634839490352">
+<node TEXT="includes" ID="ID_256489941" CREATED="1634839795803" MODIFIED="1634839814603">
+<node TEXT="Event(s)" ID="ID_1796216758" CREATED="1634839820140" MODIFIED="1634839826052"/>
+<node TEXT="Error(s)" ID="ID_100727597" CREATED="1634839827482" MODIFIED="1634839832391"/>
+<node TEXT="Storage Items" ID="ID_451256707" CREATED="1634839836898" MODIFIED="1634839846148"/>
+<node TEXT="Dispatchable Functions" ID="ID_1958699189" CREATED="1634839847187" MODIFIED="1634839853467"/>
+</node>
+</node>
+</node>
+<node TEXT="Additional set" ID="ID_460295258" CREATED="1634840824260" MODIFIED="1634840848442">
+<node TEXT="Call" ID="ID_1815584465" CREATED="1634840850585" MODIFIED="1634840857416"/>
+<node TEXT="Origin" ID="ID_1761248642" CREATED="1634840861449" MODIFIED="1634840864759"/>
+<node TEXT="Index" ID="ID_1308477999" CREATED="1634840865157" MODIFIED="1634840867037"/>
+<node TEXT="Hashing" ID="ID_1521634011" CREATED="1634840867741" MODIFIED="1634840870940"/>
+<node TEXT="AccountId" ID="ID_813710444" CREATED="1634840871275" MODIFIED="1634840874535"/>
+<node TEXT="Event" ID="ID_224794553" CREATED="1634840874884" MODIFIED="1634840876493"/>
+<node TEXT="Version" ID="ID_1883864500" CREATED="1634840876864" MODIFIED="1634840878617"/>
+</node>
+</node>
+</node>
+<node TEXT="Concepts" POSITION="right" ID="ID_254231660" CREATED="1634844814403" MODIFIED="1634844821897">
+<node TEXT="Account Abstractions" ID="ID_264040314" CREATED="1634844866673" MODIFIED="1634844882908">
+<node TEXT="keys" ID="ID_1380575467" CREATED="1634848963998" MODIFIED="1634848966329">
+<node TEXT="Stash keys" ID="ID_1026641142" CREATED="1634848967713" MODIFIED="1634848976171"/>
+<node TEXT="Controller keys" ID="ID_426950671" CREATED="1634848977303" MODIFIED="1634848981618"/>
+</node>
+</node>
+</node>
+</node>
+</map>
